8dda1e4a442483e266afb1b38d377ac3924e86ac
BerkanKaya <99059255@mydavinci.nl>
2019-09-10 11:58:14 +0200

    opdracht 1

---
+++ b1w2l1.js
@@ -0,0 +1,8 @@
+var naam = prompt('voer uw naam in:');
+var achternaam = prompt('voer uw achternaam in:');
+var leeftijd = prompt('voer uw leeftijd in:');
+var opleiding = prompt('Wat doet u als opleiding:');
+document.write('Voornaam: ' + naam);
+document.write(' Achternaam: ' + achternaam);
+document.write(' leeftijd: ' + leeftijd);
+document.write(' opleiding: ' + opleiding);
